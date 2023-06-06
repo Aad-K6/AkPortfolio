0b33bcc8c04983557568acb4253e841ba0786d3a
Kirtiraj <refreshmymind0@gmail.com>
2023-06-06 18:46:22 +0530

    text style fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="2" className="footer-copywright">
-          <h3 style={{ color: "blue" }}>© 2022-2023 Aaditi Kshirsagar</h3>
+          <h3>© 2022-2023 Aaditi Kshirsagar</h3>
         </Col>
       </Row>
     </Container>
